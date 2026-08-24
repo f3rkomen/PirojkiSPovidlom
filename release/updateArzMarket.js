c1beb3559139dd264956dd4f8a937fc8545b1ad0
ferkomen <sbn5nnt8jb@privaterelay.appleid.com>
2026-08-24 22:55:33 +0300

    Update updateArzMarket.js

--- release/updateArzMarket.js
+++ release/updateArzMarket.js
@@ -2,7 +2,7 @@
   "updateurl": "https://raw.githubusercontent.com/f3rkomen/PirojkiSPovidlom/main/release/ArzMarket.lua",
   "latest": "3.56-fork.1",
   "itemsUpdate": 31,
-  "sha256": "b6854a312e35aa49740b66317bae7d93597606b04dd85e144b0d9672c5bd6e23",
+  "sha256": "e3a61054fb2547cbb0501d18ce598ba16bbff6d6f775214a01926b33087e81f3",
   "channel": "manual-review",
   "loaderLatest": "1.2.2",
   "loaderUpdateUrl": "https://raw.githubusercontent.com/f3rkomen/PirojkiSPovidlom/main/release/ArzMarket_Loader.lua",
